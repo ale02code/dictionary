18dde754f5aca7fde76b9b108937a2ad0a01d2ac
Christopher <114704469+Blade-24@users.noreply.github.com>
2024-03-25 15:12:59 -0600

    first version working

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ Users should be able to:
 
 -Search for words using the input field ❌
 -See the Free Dictionary API's response for the searched word ❌
--See a form validation message when trying to submit a blank form ❌
+-See a form validation message when trying to submit a blank form ✅
 -Play the audio file for a word when it's available ❌
 -Switch between serif, sans serif, and monospace fonts ❌
 -Switch between light and dark themes ❌
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,3 +1,5 @@
+import React, { useState } from "react";
+
 import searchImg from "../assets/images/icon-search.svg";
 import { useContext } from "react";
 import { SearchContext } from "../context/Search";
@@ -5,23 +7,34 @@ import { SearchContext } from "../context/Search";
 function SearchBar({ text }) {
   const { setSearch } = useContext(SearchContext);
 
-  const handleSearch = (e) => {
-    setSearch(e.target.value);
+  const [wordValue, setWordValue] = useState("");
+
+  const handleChangeWord = (e) => {
+    setWordValue(e.target.value);
+  };
+
+  const handleSendWord = () => {
+    setSearch(wordValue);
+    setWordValue("");
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") handleSendWord();
   };
 
   return (
     <section className="w-full flex justify-start items-center relative mb-4">
       <input
         type="text"
-        // name=""
-        // id=""
         placeholder={text}
         className="w-90 outline-none bg-neutral-200 text-xl py-2 px-3 rounded-l-xl"
         spellCheck="false"
-        onChange={handleSearch}
+        onChange={handleChangeWord}
+        onKeyDown={handleKeyDown}
+        value={wordValue}
       />
       <div className="h-full flex justify-center items-center absolute right-0 bg-neutral-200 w-[11%] rounded-r-xl z-10">
-        <img src={searchImg} alt="search-icon" />
+        <img src={searchImg} alt="search-icon" onClick={handleSendWord} />
       </div>
     </section>
   );
